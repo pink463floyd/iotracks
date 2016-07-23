bd5be853a00bcb3d62663ff2cbceb91993800d34
slarribeau <slarribeau@gmail.com>
2016-07-22 19:39:02 -0700

    Fix typos

--- ping_send/ping_send.py
+++ ping_send/ping_send.py
@@ -1,8 +1,13 @@
 import json
+print("1")
 import iofabric.client
+print("2")
 import iofabric.iomessage
+print("3")
 import os
+print("4")
 import time
+print("5")
 
 confg=None
 msgClient=None
@@ -30,20 +35,34 @@ class IoFabricListener:
    config=new_config;
 
 
-host = iofabric.client.get_host();
-listener = IoFabricListener();
-msgClient = iofabric.client.Client("ws://" + host + ":54321/v2/control/socket/id" + CONTAINER_ID, listener, CONTAINER_ID);
-msgClient.connect();
-ctlClient = iofabric.client.Client("ws://" + host + ":54321/v2/control/socket/id" + CONTAINER_ID, listener, CONTAINER_ID);
-ctlClient.connect();
+print("6")
 
+host = iofabric.client.get_host();
+print("7")
+print(host);
+print("8")
+
+listener = IoFabricListener();
+print("9")
+uri = "ws://" + host + ":54321/v2/control/socket/id/" + CONTAINER_ID;
+print(uri);
+msgClient = iofabric.client.Client(uri, listener, CONTAINER_ID);
+print("10")
+msgClient.connect();
+print("11")
+ctlClient = iofabric.client.Client("ws://" + host + ":54321/v2/control/socket/id/" + CONTAINER_ID, listener, CONTAINER_ID);
+print("12")
+ctlClient.connect();
+print("13")
 
 
 content ="{\"payload\" : \"abcd\"}";
 while True:
     print(content);
     time.sleep(2);
-    msg=iofabric.iomesssage.IoMessage();
+
+    msg=iofabric.iomessage.IoMessage();
     msg.infotype="ctrl/ping";
     msg.infoformat="integer/Byte";
     msg.contentdata=content;
+
